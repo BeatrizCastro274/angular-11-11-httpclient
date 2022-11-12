baf7657805617b1887ef8f0576bf2a090db5b08d
BeatrizCastro274 <111613357+BeatrizCastro274@users.noreply.github.com>
2022-11-11 21:06:10 -0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,28 +4,30 @@ import { BitcoinService } from './bitcoin.service';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'BitCoin$';
+export class AppComponent {
+  name = 'Beatriz Castro RA: 50831811004';
 
   counter = 0;
   timer = setInterval(() => {
     this.counter = this.counter + 1;
   }, 100);
 
-
-  constructor(public bitcoinService: BitcoinService){ }
+  constructor(public bitcoinService: BitcoinService) {}
 
   ngOnInit() {
     this.update();
   }
 
-  getCurrentPrice(){
+  getCurrentPrice() {
     return this.bitcoinService.currentPrice;
   }
 
   update() {
     this.bitcoinService.update();
+    if (this.bitcoinService.currentPrice === this.bitcoinService.currentPrice) {
+      this.bitcoinService.update();
+    }
   }
 }
