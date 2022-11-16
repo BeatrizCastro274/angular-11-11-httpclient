9c1f415140ee91283675db4b8fbf01f020a38dcc
BeatrizCastro274 <111613357+BeatrizCastro274@users.noreply.github.com>
2022-11-15 21:46:33 -0300

    1 file modified

--- src/app/bitcoin.service.ts
+++ src/app/bitcoin.service.ts
@@ -1,47 +1,42 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 interface Response {
   time: {
     updated: string;
   };
-  disclaimer: string;
   bpi: {
-    [key in 'USD' | 'BRL' ]: {
-      symbol: string;
-      description: string;
-      rate_float: number;
+    USD: {
+      code: string;
       rate: string;
+      rate_float: number;
+    };
+    BRL: {
+      code: string;
+      rate: string;
+      rate_float: number;
     };
   };
 }
 
-interface PriceUpdate {
-  timestamp: Date;
-  USD: number;
-  BRL : number;
-  
-}
-
 @Injectable()
 export class BitcoinService {
-  currentPrice: Response;
-  lastUpdate: Date;
-
-  updateList: Array<PriceUpdate> = [];
   constructor(private http: HttpClient) {}
+  currentResponse: Response;
+  updateList: Array<Response> = [];
 
   update() {
     this.http
-      .get<Response>('https://api.coindesk.com/v1/bpi/currentprice/BRL.json')
-      .subscribe((data) => {
-        this.lastUpdate = new Date();
-        this.currentPrice = data;
-        this.updateList.push({
-          timestamp: this.lastUpdate,
-          USD: this.currentPrice.bpi.USD.rate_float,
-          BRL: this.currentPrice.bpi.BRL.rate_float,
-        });
+      .get<Response>("https://api.coindesk.com/v1/bpi/currentprice/BRL.json")
+      .subscribe(data => {
+        if (!this.currentResponse) {
+          this.updateList.push(data);
+        } else if (
+          data.bpi.BRL.rate_float !== this.currentResponse.bpi.BRL.rate_float
+        ) {
+          this.updateList.push(data);
+        }
+        this.currentResponse = data;
       });
   }
 }
